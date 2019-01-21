faf76cc2e91d1a3c60cdf4ad3ec74c9376a003ad
Michele Caini <michele.caini@gmail.com>
2019-01-22 00:08:54 +0100

    wip

--- src/entt/entity/policy.hpp
+++ src/entt/entity/policy.hpp
@@ -34,6 +34,14 @@ class registry;
  */
 
 
+/**
+ * @brief TODO
+ *
+ * TODO
+ *
+ * @tparam Entity TODO
+ * @tparam Component TODO
+ */
 template<typename Entity, typename... Component>
 class policy {
     static_assert(sizeof...(Component));
@@ -61,36 +69,86 @@ class policy {
     {}
 
 public:
+    /*! @brief TODO */
     using entity_type = typename sparse_set<Entity>::entity_type;
+    /*! @brief TODO */
     using size_type = typename sparse_set<entity_type>::size_type;
+    /*! @brief TODO */
     using iterator_type = typename sparse_set<entity_type>::iterator_type;
 
+    /*! @brief TODO */
     policy(const policy &) = default;
+    /*! @brief TODO */
     policy(policy &&) = default;
 
+    /*! @brief TODO @return TODO */
     policy & operator=(const policy &) = default;
+    /*! @brief TODO @return TODO */
     policy & operator=(policy &&) = default;
 
+    /**
+     * @brief TODO
+     *
+     * TODO
+     *
+     * @return TODO
+     */
     size_type size() const ENTT_NOEXCEPT {
         return length;
     }
 
+    /**
+     * @brief TODO
+     *
+     * TODO
+     *
+     * @return TODO
+     */
     bool empty() const ENTT_NOEXCEPT {
         return length;
     }
 
+    /**
+     * @brief TODO
+     *
+     * TODO
+     *
+     * @return TODO
+     */
     const entity_type * data() const ENTT_NOEXCEPT {
         return direct->data() + direct->size() - base - length;
     }
 
+    /**
+     * @brief TODO
+     *
+     * TODO
+     *
+     * @return TODO
+     */
     iterator_type begin() const ENTT_NOEXCEPT {
         return direct->begin() + base;
     }
 
+    /**
+     * @brief TODO
+     *
+     * TODO
+     *
+     * @return TODO
+     */
     iterator_type end() const ENTT_NOEXCEPT {
         return direct->begin() + base + length;
     }
 
+    /**
+     * @brief TODO
+     *
+     * TODO
+     *
+     * @param entity TODO
+     * @return TODO
+     */
     iterator_type find(const entity_type entity) const ENTT_NOEXCEPT {
         const auto it = direct->find(entity);
         const auto from = begin();
@@ -98,14 +156,39 @@ public:
         return (it >= from && it < to && *it == entity) ? it : to;
     }
 
+    /**
+     * @brief TODO
+     *
+     * TODO
+     *
+     * @param pos TODO
+     * @return TODO
+     */
     entity_type operator[](const size_type pos) const ENTT_NOEXCEPT {
         return (direct->begin()+base)[pos];
     }
 
+    /**
+     * @brief TODO
+     *
+     * TODO
+     *
+     * @param entity TODO
+     * @return TODO
+     */
     bool contains(const entity_type entity) const ENTT_NOEXCEPT {
         return !(find(entity) == end());
     }
 
+    /**
+     * @brief TODO
+     *
+     * TODO
+     *
+     * @tparam Comp TODO
+     * @param entity TODO
+     * @return TODO
+     */
     template<typename... Comp>
     std::conditional_t<sizeof...(Comp) == 1, std::tuple_element_t<0, std::tuple<Comp &...>>, std::tuple<Comp &...>>
     get([[maybe_unused]] const entity_type entity) const ENTT_NOEXCEPT {
@@ -119,6 +202,15 @@ public:
         }
     }
 
+    /**
+     * @brief TODO
+     *
+     * TODO
+     *
+     * @tparam Func TODO
+     * @param func TODO
+     * @return TODO
+     */
     template<typename Func>
     inline void each(Func func) const {
         auto raw = std::make_tuple((std::get<pool_type<Component> *>(pools)->begin()+base)...);
@@ -144,114 +236,6 @@ private:
     offset_type base;
     offset_type length;
 };
-
-
-// template<typename, typename>
-// class policy;
-//
-//
-// template<typename Entity, typename... Induce>
-// class policy<Entity, type_list<Induce...>> {
-//     static_assert(sizeof...(Induce));
-//
-//     /*! @brief A registry is allowed to create views. */
-//     friend class registry<Entity>;
-//
-//     template<typename Comp>
-//     using pool_type = std::conditional_t<std::is_const_v<Comp>, const sparse_set<Entity, std::remove_const_t<Comp>>, sparse_set<Entity, Comp>>;
-//
-//     template<typename Comp>
-//     using component_iterator_type = decltype(std::declval<pool_type<Comp>>().begin());
-//
-//     // we could use pool_type<T> *..., but vs complains about it and refuses to compile for unknown reasons (likely a bug)
-//     policy(typename sparse_set<Entity>::size_type length, pool_type<Induce> *... pools) ENTT_NOEXCEPT
-//         : length{length},
-//           pools{pools...}
-//     {}
-//
-// public:
-//     using entity_type = typename sparse_set<Entity>::entity_type;
-//     using size_type = typename sparse_set<entity_type>::size_type;
-//     using iterator_type = typename sparse_set<entity_type>::iterator_type;
-//
-//     policy(const policy &) = default;
-//     policy(policy &&) = default;
-//
-//     policy & operator=(const policy &) = default;
-//     policy & operator=(policy &&) = default;
-//
-//     size_type size() const ENTT_NOEXCEPT {
-//         return length;
-//     }
-//
-//     bool empty() const ENTT_NOEXCEPT {
-//         return length;
-//     }
-//
-//     const entity_type * data() const ENTT_NOEXCEPT {
-//         const auto *cpool = std::get<0>(pools);
-//         return cpool->data() + cpool->size() - length;
-//     }
-//
-//     iterator_type begin() const ENTT_NOEXCEPT {
-//         std::get<0>(pools)->sparse_set<entity_type>::begin();
-//     }
-//
-//     iterator_type end() const ENTT_NOEXCEPT {
-//         std::get<0>(pools)->sparse_set<entity_type>::begin() + length;
-//     }
-//
-//     iterator_type find(const entity_type entity) const ENTT_NOEXCEPT {
-//         const auto it = std::get<0>(pools)->find(entity);
-//         return (it < end() && *it == entity) ? it : end();
-//     }
-//
-//     entity_type operator[](const size_type pos) const ENTT_NOEXCEPT {
-//         return std::get<0>(pools)->sparse_set<entity_type>::begin()[pos];
-//     }
-//
-//     bool contains(const entity_type entity) const ENTT_NOEXCEPT {
-//         const sparse_set<entity_type> *cpool = std::get<0>(pools);
-//         return cpool->has(entity) && (cpool->data()[cpool->get(entity)] == entity) && !(cpool->get(entity) < (cpool->size() - length));
-//     }
-//
-//     template<typename... Comp>
-//     std::conditional_t<sizeof...(Comp) == 1, std::tuple_element_t<0, std::tuple<Comp &...>>, std::tuple<Comp &...>>
-//     get([[maybe_unused]] const entity_type entity) const ENTT_NOEXCEPT {
-//         assert(contains(entity));
-//
-//         if constexpr(sizeof...(Comp) == 1) {
-//             static_assert(std::disjunction_v<std::is_same<Comp..., Induce>..., std::is_same<std::remove_const_t<Comp>..., Induce>...>);
-//
-//             // TODO
-//         } else {
-//             // TODO
-//         }
-//     }
-//
-//     template<typename Func>
-//     inline void each(Func func) const {
-//         auto raw = std::make_tuple(std::get<pool_type<Induce> *>(pools)->begin()...);
-//
-//         if constexpr(std::is_invocable_v<Func, std::add_lvalue_reference_t<Induce>...>) {
-//             // TODO
-//             // for(size_type i = 0; i < length; ++i) {
-//             //     func(*(std::get<component_iterator_type<Induce>>(raw)++)...);
-//             // }
-//         } else {
-//             // TODO
-//             // auto begin = std::get<0>(pools)->sparse_set<Entity>::begin();
-//             //
-//             // for(size_type i = 0; i < length; ++i) {
-//             //     func(*(begin++), *(std::get<component_iterator_type<Induce>>(raw)++)...);
-//             // }
-//         }
-//     }
-//
-// private:
-//     size_type length;
-//     std::tuple<pool_type<Induce> *...> pools;
-// };
 
 
 }
